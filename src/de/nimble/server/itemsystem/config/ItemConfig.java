22f0cb44529a88a6a9ddbed09a509d42d85b7f40
nimbl0 <nimble0@protonmail.com>
2020-05-28 23:30:45 +0100

    added singleton

--- src/de/nimble/server/itemsystem/config/ItemConfig.java
+++ src/de/nimble/server/itemsystem/config/ItemConfig.java
@@ -3,8 +3,18 @@ package de.nimble.server.itemsystem.config;
 import de.nimble.server.config.Config;
 
 public class ItemConfig extends Config {
-
-	private ItemConfig(String fileName) {
+	
+	private static ItemConfig config = null;
+	
+	public static ItemConfig getInstance() {
+		if(config == null) {
+			config = new ItemConfig();
+		}
+		
+		return config;
+	}
+	
+	private ItemConfig() {
 		super("ItemConfig");
 	}
 	
