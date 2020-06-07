ac09da688d48d610a9ed47d719ddca00048a5398
nimbl0 <nimble0@protonmail.com>
2020-06-07 02:39:24 +0100

    added LoreParser to get the different lore components

--- src/de/nimble/server/parser/LoreParser.java
+++ src/de/nimble/server/parser/LoreParser.java
@@ -1,25 +1,64 @@
 package de.nimble.server.parser;
 
+import de.nimble.server.enchantmentsystem.enchants.types.EnchantmentType;
+import org.bukkit.inventory.ItemStack;
+
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
-public class LoreParser extends Parser<List<String>> {
-	
-	public LoreParser(List<String> lore) {
-		setToParse(lore);
-	}
-	
-	public LoreParser(String... items) {
-		setToParse(Arrays.asList(items));
-	}
-	
-	@Override
-	public void parse() {
-		
-	}
-	
-	public void setLore(String... items) {
-		setToParse(Arrays.asList(items));
-	}
-	
+/**
+ * parser to get the different components of the lore
+ *
+ * @author master
+ */
+public class LoreParser {
+
+  private List<String> lore;
+  private HashMap<String, Object> values;
+
+  public LoreParser(List<String> lore) {
+    this.values = new HashMap<>();
+    this.lore = lore;
+    loadComponentsToMap();
+  }
+
+  public LoreParser(String... items) {
+    this.values = new HashMap<>();
+    this.lore = Arrays.asList(items);
+    loadComponentsToMap();
+  }
+
+  public LoreParser(ItemStack item) {
+    this.values = new HashMap<>();
+    this.lore = item.getItemMeta().getLore();
+    loadComponentsToMap();
+  }
+
+  private void loadComponentsToMap() {
+    for (String line : lore) {
+      if (line.contains("ID")) {
+        String[] splittedLine = line.split(": ");
+        String id = splittedLine[1];
+        values.put("id", id);
+      } else if (line.contains("Type")) {
+        String[] splittedLine = line.split(": ");
+        values.put("type", splittedLine[1]);
+      } else {
+        values.put("description", line);
+      }
+    }
+  }
+
+  public int getID() {
+    return Integer.parseInt(String.valueOf(values.get("id"))); // ugly af
+  }
+
+  public String getDescription() {
+    return (String) values.get("description");
+  }
+
+  public EnchantmentType getType() {
+    return (EnchantmentType) values.get("type");
+  }
 }
