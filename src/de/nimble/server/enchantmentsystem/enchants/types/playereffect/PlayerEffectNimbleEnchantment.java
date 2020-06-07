400f8601c2598fe0c76a0d7e753b79c882ff4976
nimbl0 <nimble0@protonmail.com>
2020-06-07 13:22:39 +0100

    removed javadocs from getter and setter, multiplier is now loaded and saved in database

--- src/de/nimble/server/enchantmentsystem/enchants/types/playereffect/PlayerEffectNimbleEnchantment.java
+++ src/de/nimble/server/enchantmentsystem/enchants/types/playereffect/PlayerEffectNimbleEnchantment.java
@@ -2,35 +2,17 @@ package de.nimble.server.enchantmentsystem.enchants.types.playereffect;
 
 import de.nimble.server.NimbleServer;
 import de.nimble.server.enchantmentsystem.enchants.NimbleEnchantment;
-import de.nimble.server.sql.NimbleConnection;
 
 public abstract class PlayerEffectNimbleEnchantment extends NimbleEnchantment {
 
-  public PlayerEffectNimbleEnchantment() {
-    super();
-  }
-
-  /**
-   * reads multiplier from config with the given enchantmentname
-   *
-   * @return multiplier as double
-   */
   public double getMultiplier() {
     return Double.parseDouble(
-        NimbleServer.userEnchantmentsSql.get(
-            NimbleConnection.getConnection("UserEnchantments"), "multiplier", getID()));
+        String.valueOf(
+            NimbleServer.userEnchantmentsSql.get("customenchantments", "multiplier", getID())));
   }
 
-  /**
-   * set multiplier in config with the given enchantmentname
-   *
-   * @param multiplier
-   */
   public void setMultiplier(double multiplier) {
     NimbleServer.userEnchantmentsSql.update(
-        NimbleConnection.getConnection("UserEnchantments"),
-        "multiplier",
-        getID(),
-        String.valueOf(multiplier));
+        "customenchantments", "multiplier", getID(), String.valueOf(multiplier));
   }
 }
