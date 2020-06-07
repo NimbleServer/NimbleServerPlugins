611afaecc02b1b1e742fed1696b42def79413a3f
nimbl0 <nimble0@protonmail.com>
2020-06-07 02:30:03 +0100

    added builder functionalities to build NimbleEnchantment

--- src/de/nimble/server/enchantmentsystem/enchants/EnchantmentBuilder.java
+++ src/de/nimble/server/enchantmentsystem/enchants/EnchantmentBuilder.java
@@ -1,92 +1,65 @@
 package de.nimble.server.enchantmentsystem.enchants;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-
-import de.nimble.server.NimbleServer;
-import de.nimble.server.customtags.NimbleTag;
+import de.nimble.server.enchantmentsystem.enchants.types.DefaultNimbleEnchantment;
+import de.nimble.server.enchantmentsystem.enchants.types.EnchantmentType;
+import de.nimble.server.enchantmentsystem.enchants.types.playereffect.DamageNimbleEnchantment;
+import de.nimble.server.enchantmentsystem.enchants.types.playereffect.HealNimbleEnchantment;
 
 /**
- * class to get custom enchantment books
+ * Builder for creating custom enchantments
  * @author master
- *
  */
 public class EnchantmentBuilder {
-	
-	private Enchantment enchantment;
-	private Material material;
-	
-	private String enchantmentName;
-	
-	public EnchantmentBuilder(String enchantmentName) {
-		this.enchantmentName = enchantmentName;
-		this.enchantment = NimbleServer.userEnchantmentConfig.getEnchantmentByName(enchantmentName);
-	}
-	
-	/**
-	 * enchantment in item shape
-	 * @return itemstack with finished enchantment
-	 */
-	public ItemStack createEnchantmentBook() {
-		ItemStack item = new ItemStack(material);
-		ItemMeta meta = item.getItemMeta();
-		meta.setDisplayName(enchantment.getDisplayName());
-		
-		// NBT ID
-		NimbleTag tag = new NimbleTag("id", enchantment.getID());
-		item = tag.create(item);
-		enchantment.addTag(tag);
-		
-		List<String> lore = new ArrayList<String>();
-		lore.add("Type: " + NimbleServer.userEnchantmentConfig.getType(enchantmentName));
-		lore.add(enchantment.getDescription());
-		
-		// String id = NimbleServer.userEnchantmentConfig.getEnchantmentByName(enchantmentName).getTag("id").getValue();
-		lore.add("ID: " + tag.getValue());
-		
-		meta.setLore(lore);
-		
-		item.setItemMeta(meta);
-		
-		return item;
-	}
-	
-	public EnchantmentBuilder setEnchantment() {
-		if(this.enchantmentName != null && (!(this.enchantmentName.equals("")))) {
-			this.enchantment = NimbleServer.userEnchantmentConfig.getEnchantmentByName(this.enchantmentName);
+
+	private int id;
+	private EnchantmentType type;
+	private String displayName;
+	private String description;
+
+	public NimbleEnchantment build() {
+		NimbleEnchantment enchantment = null;
+
+		switch (type) {
+			case DAMAGE:
+				enchantment = new DamageNimbleEnchantment();
+				break;
+			case HEAL:
+				enchantment = new HealNimbleEnchantment();
+				break;
+			case NONE:
+				enchantment = new DefaultNimbleEnchantment();
+				break;
+			default:
+				break;
 		}
+
+		enchantment.setID(id);
+		enchantment.setDescription(description);
+		enchantment.setDisplayName(displayName);
+		enchantment.setDescription(description);
+		enchantment.setType(type);
+
+		return enchantment;
+	}
+
+	public EnchantmentBuilder id(int id) {
+		this.id = id;
 		return this;
 	}
-	
-	public EnchantmentBuilder setEnchantment(String enchantmentName) {
-		this.enchantment = NimbleServer.userEnchantmentConfig.getEnchantmentByName(enchantmentName);
+
+	public EnchantmentBuilder type(EnchantmentType type) {
+		this.type = type;
 		return this;
 	}
-	
-	public Enchantment getEnchantment() {
-		return this.enchantment;
-	}
-	
-	public EnchantmentBuilder setMaterial(Material material) {
-		this.material = material;
+
+	public EnchantmentBuilder displayName(String displayName) {
+		this.displayName = displayName;
 		return this;
 	}
-	
-	public Material getMaterial() {
-		return this.material;
-	}
-	
-	public EnchantmentBuilder setEnchantmentName(String enchantmentName) {
-		this.enchantmentName = enchantmentName;
+
+	public EnchantmentBuilder description(String description) {
+		this.description = description;
 		return this;
 	}
-	
-	public String getEnchantmentName() {
-		return this.enchantmentName;
-	}
-	
+
 }
