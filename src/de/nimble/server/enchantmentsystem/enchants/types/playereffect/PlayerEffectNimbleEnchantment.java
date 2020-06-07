f617748cbba8c2432bf54ce0d17859dad548d908
nimbl0 <nimble0@protonmail.com>
2020-06-07 02:32:27 +0100

    removed enchantmentname from super call

--- src/de/nimble/server/enchantmentsystem/enchants/types/playereffect/PlayerEffectNimbleEnchantment.java
+++ src/de/nimble/server/enchantmentsystem/enchants/types/playereffect/PlayerEffectNimbleEnchantment.java
@@ -1,30 +1,36 @@
 package de.nimble.server.enchantmentsystem.enchants.types.playereffect;
 
 import de.nimble.server.NimbleServer;
-import de.nimble.server.enchantmentsystem.config.UserEnchantmentSQL;
-import de.nimble.server.enchantmentsystem.enchants.EnchantmentSql;
-import de.nimble.server.mysql.NimbleConnection;
+import de.nimble.server.enchantmentsystem.enchants.NimbleEnchantment;
+import de.nimble.server.sql.NimbleConnection;
 
-public abstract class PlayerEffectEnchantment extends EnchantmentSql {
-	
-	public PlayerEffectEnchantment(String enchantmentName) {
-		super(enchantmentName);
-	}
+public abstract class PlayerEffectNimbleEnchantment extends NimbleEnchantment {
 
-	/**
-	 * reads multiplier from config with the given enchantmentname
-	 * @return multiplier as double
-	 */
-	public double getMultiplier() {
-		return Double.parseDouble(NimbleServer.userEnchantmentsSql.get(NimbleConnection.getConnection(UserEnchantmentSQL.DB_NAME), "multiplier", getEnchantmentName()));
-	}
-	
-	/**
-	 * set multiplier in config with the given enchantmentname
-	 * @param multiplier
-	 */
-	public void setMultiplier(double multiplier) {
-		NimbleServer.userEnchantmentsSql.update(NimbleConnection.getConnection(UserEnchantmentSQL.DB_NAME), "multiplier", getEnchantmentName(), String.valueOf(multiplier));
-	}
-	
+  public PlayerEffectNimbleEnchantment() {
+    super();
+  }
+
+  /**
+   * reads multiplier from config with the given enchantmentname
+   *
+   * @return multiplier as double
+   */
+  public double getMultiplier() {
+    return Double.parseDouble(
+        NimbleServer.userEnchantmentsSql.get(
+            NimbleConnection.getConnection("UserEnchantments"), "multiplier", getID()));
+  }
+
+  /**
+   * set multiplier in config with the given enchantmentname
+   *
+   * @param multiplier
+   */
+  public void setMultiplier(double multiplier) {
+    NimbleServer.userEnchantmentsSql.update(
+        NimbleConnection.getConnection("UserEnchantments"),
+        "multiplier",
+        getID(),
+        String.valueOf(multiplier));
+  }
 }
