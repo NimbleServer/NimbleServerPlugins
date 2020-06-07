d7bc6f14170d24b14ee0ee7e2e3e89f4152ea617
nimbl0 <nimble0@protonmail.com>
2020-06-07 02:41:40 +0100

    default enchantment if no type is given

---
+++ src/de/nimble/server/enchantmentsystem/enchants/types/DefaultNimbleEnchantment.java
@@ -0,0 +1,14 @@
+package de.nimble.server.enchantmentsystem.enchants.types;
+
+import de.nimble.server.enchantmentsystem.enchants.NimbleEnchantment;
+import org.bukkit.event.Event;
+
+public class DefaultNimbleEnchantment extends NimbleEnchantment {
+
+  public DefaultNimbleEnchantment() {
+    super();
+  }
+
+  @Override
+  public void onUse(Event event) {}
+}
