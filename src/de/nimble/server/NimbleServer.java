9cf5177db3417151b91307af7362777db7a8b7c8
nimbl0 <nimble0@protonmail.com>
2020-06-07 13:15:59 +0100

    added AttackEvent, managers and configs for items and modified onEnable/disable message

--- src/de/nimble/server/NimbleServer.java
+++ src/de/nimble/server/NimbleServer.java
@@ -1,35 +1,35 @@
 package de.nimble.server;
 
+import de.nimble.server.itemsystem.commands.NimbleItemCommand;
+import de.nimble.server.itemsystem.config.NimbleItemSql;
+import de.nimble.server.itemsystem.events.AttackEvent;
+import de.nimble.server.itemsystem.items.ItemManager;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import de.nimble.server.enchantmentsystem.commands.EnchantmentCommand;
 import de.nimble.server.enchantmentsystem.config.UserEnchantmentSQL;
 import de.nimble.server.enchantmentsystem.enchants.EnchantmentManager;
-import de.nimble.server.itemsystem.config.ItemConfig;
-import de.nimble.server.itemsystem.config.NimbleItemConfig;
 
 public class NimbleServer extends JavaPlugin {
 
   // Manager
   public static EnchantmentManager enchantmentManager = null;
+  public static ItemManager itemManager = null;
 
   // Enchantment configs
   public static UserEnchantmentSQL userEnchantmentsSql = null;
 
-  /*
-   * Item configs
-   */
-  public static NimbleItemConfig nimbleItemConfig = null;
-  public static ItemConfig itemConfig = null;
+  // Item configs
+  public static NimbleItemSql itemSql = null;
 
   public void onEnable() {
     init();
-    System.out.println("EnchantmentSystem loaded");
+    System.out.println("NimbleServer loaded");
   }
 
   public void onDisable() {
-    System.out.println("EnchantmentSystem disabled");
+    System.out.println("NimbleServer disabled");
   }
 
   private void init() {
@@ -39,12 +39,16 @@ public class NimbleServer extends JavaPlugin {
     loadConfigFiles();
     loadManager();
     initManagerContent();
+    NimbleLogger.getInstance().setServer(this.getServer());
   }
 
-  private void registerEvents(PluginManager pm) {}
+  private void registerEvents(PluginManager pm) {
+    pm.registerEvents(new AttackEvent(), this);
+  }
 
   private void loadCommands() {
     getCommand("enchantments").setExecutor(new EnchantmentCommand());
+    getCommand("items").setExecutor(new NimbleItemCommand());
   }
 
   private void loadConfig() {
@@ -56,15 +60,24 @@ public class NimbleServer extends JavaPlugin {
     // enchantment configs
     userEnchantmentsSql = UserEnchantmentSQL.getInstance();
     // item configs
-    nimbleItemConfig = NimbleItemConfig.getInstance();
+    itemSql = NimbleItemSql.getInstance();
   }
 
   private void loadManager() {
     enchantmentManager = EnchantmentManager.getInstance();
+    itemManager = ItemManager.getInstance();
   }
 
   private void initManagerContent() {
     // custom enchantments
     userEnchantmentsSql.getEnchantments().forEach(e -> enchantmentManager.addEnchantment(e));
+    // custom items
+    itemSql
+        .getItems()
+        .forEach(
+            i -> {
+              System.out.println(i.getID());
+              itemManager.addItem(i);
+            });
   }
 }
---
+++ src/de/nimble/server/config/SQLConfig.java
@@ -0,0 +1,93 @@
+package de.nimble.server.enchantmentsystem.config;
+
+import de.nimble.server.sql.NimbleConnection;
+
+import java.sql.*;
+
+public abstract class SQLConfig {
+
+  private String dbName;
+  private Connection con;
+
+  public SQLConfig(String dbName) {
+    this.dbName = dbName;
+    createTable();
+  }
+
+  public abstract void createTable();
+
+  public int updateID(String tableName) {
+    Statement st = null;
+    ResultSet rs = null;
+    try {
+      st = con.createStatement();
+      rs = st.executeQuery("select max(id) from '" + tableName + "'");
+      while (rs.next()) {
+        return rs.getInt("max(id)") + 1;
+      }
+    } catch (SQLException throwables) {
+      throwables.printStackTrace();
+    } finally {
+      try {
+        rs.close();
+        st.close();
+      } catch (SQLException throwables) {
+        throwables.printStackTrace();
+      }
+    }
+    return -1;
+  }
+
+  public void update(String tableName, String columnName, int id, String value) {
+    PreparedStatement ps = null;
+    try {
+      ps =
+          con.prepareStatement(
+              "update " + tableName + " set '" + columnName + "' = ? where id = ?");
+      ps.setString(1, value);
+      ps.setInt(2, id);
+
+      ps.execute();
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public Object get(String tableName, String columnName, int id) {
+    try {
+      String query = "select " + columnName + " from " + tableName + " where id = ?";
+      PreparedStatement ps = con.prepareStatement(query);
+      ps.setInt(1, id);
+
+      ResultSet rs = ps.executeQuery();
+
+      while (rs.next()) {
+        return rs.getString(columnName);
+      }
+
+      rs.close();
+      ps.close();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+
+  public void initConnection() {
+    this.con = NimbleConnection.getConnection(dbName);
+  }
+
+  public Connection getConnection(String dbName) {
+    initConnection();
+    return con;
+  }
+
+  public void setDBName(String dbName) {
+    this.dbName = dbName;
+  }
+
+  public String getDBName() {
+    return this.dbName;
+  }
+}
