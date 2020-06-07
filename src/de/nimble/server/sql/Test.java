95af42b335d1fee69b719225c44fd6a305ede131
nimbl0 <nimble0@protonmail.com>
2020-06-07 02:40:04 +0100

    test shit

--- src/de/nimble/server/sql/Test.java
+++ src/de/nimble/server/sql/Test.java
@@ -1,41 +1,43 @@
-package de.nimble.server.mysql;
+package de.nimble.server.sql;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import de.nimble.server.enchantmentsystem.config.UserEnchantmentSQL;
+import java.sql.*;
 
 public class Test {
-	
-	public static void main(String[] args) {
-		Connection con = NimbleConnection.getConnection(UserEnchantmentSQL.DB_NAME);
-		try {
-			try {
-				Statement stmnt = con.createStatement();
-				stmnt.execute(
-						"create table if not exists customenchantments(id integer, enchantmentname text, displayname text, description text, type text, multiplier real)");
-				stmnt.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			
-			
-			PreparedStatement ps = con.prepareStatement("select id from customenchantments where enchantmentname = 'test'");
-			ResultSet rs = ps.executeQuery();
-			
-			while(rs.next()) {
-				System.out.println(rs.getString("id"));
-			}
-			
-			rs.close();
-			ps.close();
-			con.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	
+
+  public static void main(String[] args) {
+    Connection con = NimbleConnection.getConnection("testbla");
+    try {
+      try {
+        Statement stmnt = con.createStatement();
+        stmnt.execute(
+            "create table if not exists customenchantments(" +
+                    "id integer," +
+                    "displayname text," +
+                    "description text," +
+                    "type text," +
+                    "multiplier real)");
+        stmnt.close();
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+
+      PreparedStatement ps =
+          con.prepareStatement("insert into customenchantments values(6, 'test', 'description', 'type', 3)");
+      ps.execute();
+
+      ps = con.prepareStatement("select max(id) from customenchantments");
+
+      ResultSet rs = ps.executeQuery();
+
+      while (rs.next()) {
+        System.out.println(rs.getInt("max(id)"));
+      }
+
+      rs.close();
+      ps.close();
+      con.close();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
 }
