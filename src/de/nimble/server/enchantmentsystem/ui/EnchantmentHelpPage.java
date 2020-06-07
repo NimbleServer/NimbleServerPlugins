8f22514f3bc464d18cc2743a3fb793cd6b0824e3
nimbl0 <nimble0@protonmail.com>
2020-06-07 02:34:14 +0100

    replaced enchantmentname with id

--- src/de/nimble/server/enchantmentsystem/ui/EnchantmentHelpPage.java
+++ src/de/nimble/server/enchantmentsystem/ui/EnchantmentHelpPage.java
@@ -7,7 +7,7 @@ import de.nimble.server.ui.chat.ClickableText;
 
 public class EnchantmentHelpPage {
 	
-	// TODO show help page instead of just 
+	// TODO show help page instead of just enchantments
 	
 	/**
 	 * Shows all the enchantments and adds command to get them
@@ -17,13 +17,19 @@ public class EnchantmentHelpPage {
 	public void send(Player player) {
 		ClickableText helpPage = new ClickableText();
 		
-		NimbleServer.userEnchantmentConfig.getEnchantments().stream().forEach(enchantment -> {
-			helpPage.add(
-				enchantment.getEnchantmentName(), 
-				"/enchantments get " + enchantment.getEnchantmentName(), 
-				"Displayname: " + enchantment.getDisplayName()
-			);
-		});
+		if(NimbleServer.enchantmentManager.getEnchantments().size() == 0) {
+			helpPage.add("No enchantments", "", "No enchantments");
+		} else {
+			NimbleServer.enchantmentManager.getEnchantments().stream().forEach(enchantment -> {
+				helpPage.add(
+					enchantment.getDisplayName(),
+					"/enchantments get " + enchantment.getID(),
+					"Displayname: " + enchantment.getDisplayName()
+				);
+			});
+		}
+		
+		
 		
 		helpPage.send(player);
 	}
