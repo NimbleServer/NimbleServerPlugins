75a612f021ef4a01101a97b39084fdebb14c00d0
nimbl0 <nimble0@protonmail.com>
2020-06-07 02:40:32 +0100

    added enchantmentmanager to load, delete and get enchantments

---
+++ src/de/nimble/server/enchantmentsystem/enchants/EnchantmentManager.java
@@ -0,0 +1,56 @@
+package de.nimble.server.enchantmentsystem.enchants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EnchantmentManager {
+
+  private List<NimbleEnchantment> enchantments;
+
+  private static EnchantmentManager manager = null;
+
+  public static EnchantmentManager getInstance() {
+    if (manager == null) {
+      manager = new EnchantmentManager();
+    }
+
+    return manager;
+  }
+
+  private EnchantmentManager() {
+    this.enchantments = new ArrayList<NimbleEnchantment>();
+  }
+
+  public void addEnchantment(NimbleEnchantment enchantment) {
+    this.enchantments.add(enchantment);
+  }
+
+  public void deleteEnchantment(NimbleEnchantment enchantment) {
+    deleteEnchantment(enchantment.getID());
+  }
+
+  public void deleteEnchantment(int id) {
+    for (NimbleEnchantment enchantment : enchantments) {
+      if (enchantment.getID() == id) {
+        enchantments.remove(enchantment);
+      }
+    }
+  }
+
+  public NimbleEnchantment getEnchantment(int id) {
+    for (NimbleEnchantment enchantment : enchantments) {
+      if (enchantment.getID() == id) {
+        return enchantment;
+      }
+    }
+    return null;
+  }
+
+  public List<NimbleEnchantment> getEnchantments() {
+    return this.enchantments;
+  }
+
+  public void clear() {
+    enchantments.clear();
+  }
+}
