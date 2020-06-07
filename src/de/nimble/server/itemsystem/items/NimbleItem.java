63e15d072093beb405ebeb431c25f8c7113ec1b0
nimbl0 <nimble0@protonmail.com>
2020-06-07 13:23:36 +0100

    added all functionality and attributes to NimbleItem

--- src/de/nimble/server/itemsystem/items/NimbleItem.java
+++ src/de/nimble/server/itemsystem/items/NimbleItem.java
@@ -1,163 +1,86 @@
 package de.nimble.server.itemsystem.items;
 
+import de.nimble.server.enchantmentsystem.enchants.NimbleEnchantment;
+import de.nimble.server.itemsystem.items.types.NimbleItemType;
+import org.bukkit.Material;
+import org.bukkit.event.Event;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.bukkit.Material;
-import org.bukkit.event.Event;
-import org.bukkit.inventory.ItemStack;
-
-import de.nimble.server.NimbleServer;
-import de.nimble.server.customtags.NimbleTag;
-
 public abstract class NimbleItem {
-	
-	private String itemName;
-	
-	private ItemStack item;
-	
-	private List<Enchantment> enchantments;
-	private List<NimbleTag> tags;
-	
-	public NimbleItem() {
-		this.enchantments = new ArrayList<Enchantment>();
-		this.tags = new ArrayList<NimbleTag>();
-	}
-	
-	/**
-	 * onUse method gets implementated in class that extends Enchantment
-	 * defines what happens when item with enchantment is used
-	 * @param event specified by what should happen 
-	 */
-	public abstract void onUse(Event event);
-	
-	/**
-	 * saves id in config
-	 * @param id
-	 */
-	public void setID(String id) {
-		NimbleServer.nimbleItemConfig.setID(itemName, id);
-	}
-	
-	/**
-	 * reads id from config by the given itemname
-	 * @return String id
-	 */
-	public String getID() {
-		return NimbleServer.nimbleItemConfig.getID(itemName);
-	}
-	
-	public void setItemName(String itemName) {
-		this.itemName = itemName;
-	}
-	
-	public String getItemName() {
-		return this.itemName;
-	}
-	
-	/**
-	 * saves displayname in config by the given itemname
-	 * @param displayName
-	 */
-	public void setDisplayName(String displayName) {
-		NimbleServer.nimbleItemConfig.setDisplayName(itemName, displayName);
-	}
-	
-	/**
-	 * @return displayname for the item by the given itemname
-	 */
-	public String getDisplayName() {
-		return NimbleServer.nimbleItemConfig.getDisplayName(itemName);
-	}
-	
-	/**
-	 * saves material in config by the given itemname
-	 * @param material enum
-	 */
-	public void setMaterial(Material material) {
-		NimbleServer.nimbleItemConfig.setMaterial(itemName, material);
-	}
-	
-	/**
-	 * saves material in config by the given itemname
-	 * @param String material
-	 */
-	public void setMaterial(String material) {
-		NimbleServer.nimbleItemConfig.setMaterial(material, material);
-	}
-	
-	/**
-	 * @return material enum by the given itemname
-	 */
-	public Material getMaterial() {
-		return NimbleServer.nimbleItemConfig.getMaterial(itemName);
-	}
-	
-	/**
-	 * saves enchantment list in config
-	 * @param enchantment array
-	 */
-	public void addEnchantment(Enchantment... enchantment) {
-		Arrays.asList(enchantment).stream().forEach(e -> enchantments.add(e));
-		NimbleServer.nimbleItemConfig.setEnchantments(itemName, enchantments);
-	}
-	
-	/**
-	 * @return list of enchantments given by the itemname
-	 */
-	public List<Enchantment> getEnchantments() {
-		return NimbleServer.nimbleItemConfig.getEnchantments(itemName);
-	}
-	
-	/**
-	 * Initializes item
-	 * @param ItemStack item
-	 */
-	public void setItem(ItemStack item) {
-		this.item = item;
-	}
-	
-	/**
-	 * @return Item
-	 */
-	public ItemStack getItem() {
-		return item;
-	}
-	
-	/**
-	 * adds a customtag to the list of tags an enchantment has
-	 * @param NimbleTag
-	 */
-	public void addTag(String tagName, String tagValue) {
-		NimbleTag tag = new NimbleTag(tagName, tagValue);
-		setItem(tag.create(getItem()));
-		
-		tags.add(tag);
-		
-		NimbleServer.nimbleItemConfig.setTags(tagValue, tags);
-	}
-	
-	/**
-	 * searches a tag for its name in the list of tags
-	 * 
-	 * @param name
-	 * @return null or tag
-	 */
-	public NimbleTag getTag(String name) {
-		for(NimbleTag tag : NimbleServer.nimbleItemConfig.getTags(itemName)) {
-			if(tag.getTagName().equals(name)) {
-				return tag;
-			}
-		}
-		return null;
-	}
-	
-	/**
-	 * @return all tags read from config by the given itemname
-	 */
-	public List<NimbleTag> getTags() {
-		return NimbleServer.nimbleItemConfig.getTags(itemName);
-	}
-	
+
+  private int id;
+  private String displayName;
+  private String description;
+
+  private List<NimbleEnchantment> enchantments;
+  private Material material;
+
+  private NimbleItemType type;
+
+  public NimbleItem() {
+    this.enchantments = new ArrayList<>();
+  }
+
+  /**
+   * onUse method gets implementated in class that extends Enchantment defines what happens when
+   * item with enchantment is used
+   *
+   * @param event specified by what should happen
+   */
+  public abstract void onUse(Event event);
+
+  public void setID(int id) {
+    this.id = id;
+  }
+
+  public int getID() {
+    return this.id;
+  }
+
+  public void setDisplayName(String displayName) {
+    this.displayName = displayName;
+  }
+
+  public String getDisplayName() {
+    return this.displayName;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void addEnchantment(NimbleEnchantment... enchantment) {
+    Arrays.asList(enchantment).stream().forEach(e -> enchantments.add(e));
+  }
+
+  public List<NimbleEnchantment> getEnchantments() {
+    return this.enchantments;
+  }
+
+  public void setMaterial(Material material) {
+    this.material = material;
+  }
+
+  public void setMaterial(String material) {
+    this.material = Material.getMaterial(material);
+  }
+
+  public Material getMaterial() {
+    return this.material;
+  }
+
+  public void setType(NimbleItemType type) {
+    this.type = type;
+  }
+
+  public NimbleItemType getType() {
+    return this.type;
+  }
 }
