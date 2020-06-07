d926d3f33c2daab0d06a24f3c3185127157cba26
nimbl0 <nimble0@protonmail.com>
2020-06-07 02:24:44 +0100

    removed args, clears enchantmentlist and reloads it

--- src/de/nimble/server/enchantmentsystem/commands/EnchantmentCommand.java
+++ src/de/nimble/server/enchantmentsystem/commands/EnchantmentCommand.java
@@ -1,40 +1,59 @@
 package de.nimble.server.enchantmentsystem.commands;
 
+import java.sql.Connection;
+
+import de.nimble.server.parser.LoreParser;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import de.nimble.server.enchantmentsystem.enchants.EnchantmentBuilder;
+import de.nimble.server.NimbleServer;
+import de.nimble.server.enchantmentsystem.config.UserEnchantmentSQL;
+import de.nimble.server.enchantmentsystem.enchants.NimbleEnchantmentCreator;
+import de.nimble.server.enchantmentsystem.enchants.types.EnchantmentType;
 import de.nimble.server.enchantmentsystem.ui.EnchantmentHelpPage;
+import de.nimble.server.sql.NimbleConnection;
 
 public class EnchantmentCommand implements CommandExecutor {
-	
-	/**
-	 * command for getting enchantments
-	 */
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if(!(sender instanceof Player)) {
-			return true;
-		}
-		Player player = (Player) sender;
-		
-		if(args.length == 0) {
-			EnchantmentHelpPage helpPage = new EnchantmentHelpPage();
-			helpPage.send(player);
-		} else if(args.length == 2){
-			if(args[0].equalsIgnoreCase("get")) {
-				EnchantmentBuilder enchantmentBuilder = new EnchantmentBuilder(args[1]).setMaterial(Material.ENCHANTED_BOOK);
-				player.getInventory().addItem(enchantmentBuilder.createEnchantmentBook());
-				player.updateInventory();
-			}
-		}
-		
-		return true;
-	}
-	
-	
-	
+
+  /** command for getting enchantments */
+  @Override
+  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    if (!(sender instanceof Player)) {
+      return true;
+    }
+    Player player = (Player) sender;
+
+    if (args.length == 0) {
+      EnchantmentHelpPage helpPage = new EnchantmentHelpPage();
+      helpPage.send(player);
+    } else if (args.length == 2) {
+      if (args[0].equalsIgnoreCase("get")) {
+        NimbleEnchantmentCreator creator =
+            new NimbleEnchantmentCreator(Integer.parseInt(args[1]), Material.ENCHANTED_BOOK);
+
+        player.getInventory().addItem(creator.createEnchantment());
+        player.updateInventory();
+      }
+    } else if (args.length > 2) {
+      if (args[0].equalsIgnoreCase("create")) {
+        // solve with book and quirrel maybe?
+        Connection con = NimbleConnection.getConnection("UserEnchantments");
+        NimbleServer.userEnchantmentsSql.createNewEnchantment(
+            con,
+            args[1], // displayName
+            args[2], // description
+            EnchantmentType.getTypeByName(args[3]), // type
+            Double.parseDouble(args[4])); // multiplier
+
+        NimbleServer.enchantmentManager.clear();
+        NimbleServer.userEnchantmentsSql.getEnchantments().stream()
+            .forEach(e -> NimbleServer.enchantmentManager.addEnchantment(e));
+      }
+    }
+
+    return true;
+  }
 }
