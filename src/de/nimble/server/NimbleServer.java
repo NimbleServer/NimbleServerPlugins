c3913387cb0fcceff438066f9bb980e5a154ca21
nimbl0 <nimble0@protonmail.com>
2020-06-07 02:20:10 +0100

    removed manager instances and replaced add for enchantments

--- src/de/nimble/server/NimbleServer.java
+++ src/de/nimble/server/NimbleServer.java
@@ -4,60 +4,67 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import de.nimble.server.enchantmentsystem.commands.EnchantmentCommand;
-import de.nimble.server.enchantmentsystem.config.EnchantmentConfig;
-import de.nimble.server.enchantmentsystem.config.UserEnchantmentsConfig;
+import de.nimble.server.enchantmentsystem.config.UserEnchantmentSQL;
+import de.nimble.server.enchantmentsystem.enchants.EnchantmentManager;
 import de.nimble.server.itemsystem.config.ItemConfig;
 import de.nimble.server.itemsystem.config.NimbleItemConfig;
 
 public class NimbleServer extends JavaPlugin {
-	
-	/*
-	 * Enchantment configs
-	 */
-	public static UserEnchantmentsConfig userEnchantmentConfig = null;
-	public static EnchantmentConfig enchantmentConfig = null;
-	
-	/*
-	 * Item configs
-	 */
-	public static NimbleItemConfig nimbleItemConfig = null;
-	public static ItemConfig itemConfig = null;
-	
-	public void onEnable() {
-		init();
-		System.out.println("EnchantmentSystem loaded");
-	}
-	
-	public void onDisable() {
-		System.out.println("EnchantmentSystem disabled");
-	}
-	
-	private void init() {
-		registerEvents(getServer().getPluginManager());
-		loadCommands();
-		loadConfig();
-		loadConfigFiles();
-	}
-	
-	private void registerEvents(PluginManager pm) {
-	}
-	
-	private void loadCommands() {
-		getCommand("enchantments").setExecutor(new EnchantmentCommand());
-	}
-	
-	private void loadConfig() {
-		getConfig().options().copyDefaults(true);
-		saveConfig();
-	}
-	
-	private void loadConfigFiles() {
-		// enchantment configs
-		userEnchantmentConfig = (UserEnchantmentsConfig) UserEnchantmentsConfig.getInstance();
-		enchantmentConfig = (EnchantmentConfig) EnchantmentConfig.getInstance();
-		
-		// item configs
-		nimbleItemConfig = (NimbleItemConfig) NimbleItemConfig.getInstance();
-	}
-	
+
+  // Manager
+  public static EnchantmentManager enchantmentManager = null;
+
+  // Enchantment configs
+  public static UserEnchantmentSQL userEnchantmentsSql = null;
+
+  /*
+   * Item configs
+   */
+  public static NimbleItemConfig nimbleItemConfig = null;
+  public static ItemConfig itemConfig = null;
+
+  public void onEnable() {
+    init();
+    System.out.println("EnchantmentSystem loaded");
+  }
+
+  public void onDisable() {
+    System.out.println("EnchantmentSystem disabled");
+  }
+
+  private void init() {
+    registerEvents(getServer().getPluginManager());
+    loadCommands();
+    loadConfig();
+    loadConfigFiles();
+    loadManager();
+    initManagerContent();
+  }
+
+  private void registerEvents(PluginManager pm) {}
+
+  private void loadCommands() {
+    getCommand("enchantments").setExecutor(new EnchantmentCommand());
+  }
+
+  private void loadConfig() {
+    getConfig().options().copyDefaults(true);
+    saveConfig();
+  }
+
+  private void loadConfigFiles() {
+    // enchantment configs
+    userEnchantmentsSql = UserEnchantmentSQL.getInstance();
+    // item configs
+    nimbleItemConfig = NimbleItemConfig.getInstance();
+  }
+
+  private void loadManager() {
+    enchantmentManager = EnchantmentManager.getInstance();
+  }
+
+  private void initManagerContent() {
+    // custom enchantments
+    userEnchantmentsSql.getEnchantments().forEach(e -> enchantmentManager.addEnchantment(e));
+  }
 }
