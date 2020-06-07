e56effdb06733d298c8a19a83a5c39569be4f2ea
nimbl0 <nimble0@protonmail.com>
2020-06-07 02:31:03 +0100

    formatted with google style

--- src/de/nimble/server/enchantmentsystem/enchants/types/EnchantmentType.java
+++ src/de/nimble/server/enchantmentsystem/enchants/types/EnchantmentType.java
@@ -1,18 +1,16 @@
 package de.nimble.server.enchantmentsystem.enchants.types;
 
 public enum EnchantmentType {
-	
-	DAMAGE,
-	HEAL,
-	NONE;
-	
-	public static EnchantmentType getTypeByName(String name) {
-		for(EnchantmentType type : values()) {
-			if(name.equals(type.toString())) {
-				return type;
-			}
-		}
-		return NONE;
-	}
-	
+  DAMAGE,
+  HEAL,
+  NONE;
+
+  public static EnchantmentType getTypeByName(String name) {
+    for (EnchantmentType type : values()) {
+      if (name.equals(type.toString())) {
+        return type;
+      }
+    }
+    return NONE;
+  }
 }
