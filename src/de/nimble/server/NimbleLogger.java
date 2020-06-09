37ae0359ff2109bdf8cfe91fd240ddc897c8f719
nimbl0 <nimble0@protonmail.com>
2020-06-09 19:43:06 +0100

    added debug check and formatted with google style

--- src/de/nimble/server/NimbleLogger.java
+++ src/de/nimble/server/NimbleLogger.java
@@ -4,28 +4,27 @@ import org.bukkit.Server;
 
 public class NimbleLogger {
 
-	private final String PREFIX = "§8[§6Nimble§8] §a";
-	private Server server;
+  private final String PREFIX = "§8[§6Nimble§8] §a";
+  private Server server;
 
-	private static NimbleLogger logger = null;
+  private static NimbleLogger logger = null;
 
-	public static NimbleLogger getInstance() {
-		if(logger == null) {
-			logger = new NimbleLogger();
-		}
-		return logger;
-	}
+  public static NimbleLogger getInstance() {
+    if (logger == null) {
+      logger = new NimbleLogger();
+    }
+    return logger;
+  }
 
-	private NimbleLogger() {
+  private NimbleLogger() {}
 
-	}
+  public void log(String msg) {
+    if (Utils.DEBUG_MODE) {
+      this.server.getConsoleSender().sendMessage(PREFIX + msg);
+    }
+  }
 
-	public void log(String msg) {
-		this.server.getConsoleSender().sendMessage(PREFIX + msg);
-	}
-
-	public void setServer(Server server) {
-		this.server = server;
-	}
-
+  public void setServer(Server server) {
+    this.server = server;
+  }
 }
