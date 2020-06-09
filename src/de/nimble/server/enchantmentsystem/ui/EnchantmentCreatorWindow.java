b28d947f6c7aadcf1e19931d823d5dd0c95dbed3
nimbl0 <nimble0@protonmail.com>
2020-06-09 20:48:30 +0200

    added inventory to create new enchantments // NOT FINISHED

---
+++ src/de/nimble/server/enchantmentsystem/ui/EnchantmentCreatorWindow.java
@@ -0,0 +1,22 @@
+package de.nimble.server.enchantmentsystem.ui;
+
+import de.nimble.server.ui.window.NimbleWindow;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class EnchantmentCreatorWindow extends NimbleWindow {
+
+  public EnchantmentCreatorWindow(int size) {
+    super(size, "");
+  }
+
+
+  @Override
+  public void onClick(InventoryClickEvent event) {
+
+  }
+
+  @Override
+  public void initItems() {
+
+  }
+}
