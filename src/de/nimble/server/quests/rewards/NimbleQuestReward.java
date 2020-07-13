2c29c7fbf6607620267fb04c84e88afb495546a1
nimbl0 <nimble0@protonmail.com>
2020-07-14 00:15:21 +0200

    added attributes and getter and setter

--- src/de/nimble/server/quests/rewards/NimbleQuestReward.java
+++ src/de/nimble/server/quests/rewards/NimbleQuestReward.java
@@ -1,5 +1,43 @@
 package de.nimble.server.quests.rewards;
 
-public class NimbleQuestReward {
+public abstract class NimbleQuestReward {
 
+  private int id;
+  private int questId;
+  private String name;
+  private NimbleQuestRewardType type;
+
+  public abstract void onReward();
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public int getQuestId() {
+    return questId;
+  }
+
+  public void setQuestId(int questId) {
+    this.questId = questId;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public NimbleQuestRewardType getType() {
+    return type;
+  }
+
+  public void setType(NimbleQuestRewardType type) {
+    this.type = type;
+  }
 }
