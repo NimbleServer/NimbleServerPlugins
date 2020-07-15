1c30bb0a9b4d89843ebe28f64474a19a2309d6c0
nimbl0 <nimble0@protonmail.com>
2020-07-15 23:46:44 +0200

    added get methods and DEFAULT enum

--- src/de/nimble/server/quests/objectives/NimbleQuestObjectiveType.java
+++ src/de/nimble/server/quests/objectives/NimbleQuestObjectiveType.java
@@ -1,6 +1,9 @@
 package de.nimble.server.quests.objectives;
 
+import de.nimble.server.quests.rewards.NimbleQuestRewardType;
+
 public enum NimbleQuestObjectiveType {
+  DEFAULT((byte) 0),
   ITEM((byte) 1),
   MOB((byte) 2),
   WALK((byte) 3),
@@ -12,11 +15,27 @@ public enum NimbleQuestObjectiveType {
     this.id = id;
   }
 
-  public void setId(byte id) {
-    this.id = id;
-  }
-
   public byte getId() {
     return this.id;
+  }
+
+  public static NimbleQuestObjectiveType getById(byte id) {
+    NimbleQuestObjectiveType type = DEFAULT;
+    for (NimbleQuestObjectiveType t : values()) {
+      if (t.getId() == id) {
+        type = t;
+      }
+    }
+    return type;
+  }
+
+  public static NimbleQuestObjectiveType getByName(String name) {
+    NimbleQuestObjectiveType type = DEFAULT;
+    for (NimbleQuestObjectiveType t : values()) {
+      if (t.name().equalsIgnoreCase(name)) {
+        type = t;
+      }
+    }
+    return type;
   }
 }
