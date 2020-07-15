d70ce180c47587feb8db5874060f159276c36df0
nimbl0 <nimble0@protonmail.com>
2020-07-15 23:49:54 +0200

    added error method

--- src/de/nimble/server/NimbleLogger.java
+++ src/de/nimble/server/NimbleLogger.java
@@ -5,6 +5,7 @@ import org.bukkit.Server;
 public class NimbleLogger {
 
   private final String PREFIX = "§8[§6Nimble§8] §a";
+  private final String ERROR = "§8[§cNimbleError§8] §c";
   private Server server;
 
   private static NimbleLogger logger = null;
@@ -21,6 +22,12 @@ public class NimbleLogger {
   public void log(String msg) {
     if (Utils.DEBUG_MODE) {
       this.server.getConsoleSender().sendMessage(PREFIX + msg);
+    }
+  }
+
+  public void error(String msg) {
+    if (Utils.DEBUG_MODE) {
+      this.server.getConsoleSender().sendMessage(ERROR + msg);
     }
   }
 
