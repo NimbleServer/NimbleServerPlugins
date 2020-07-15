d172e83d685d576069e181d727b97e4d4f3873d3
nimbl0 <nimble0@protonmail.com>
2020-07-15 23:49:00 +0200

    filled Quest reward type

--- src/de/nimble/server/quests/rewards/NimbleQuestRewardType.java
+++ src/de/nimble/server/quests/rewards/NimbleQuestRewardType.java
@@ -1,4 +1,42 @@
 package de.nimble.server.quests.rewards;
 
+import java.util.Arrays;
+
 public enum NimbleQuestRewardType {
+	DEFAULT((byte) 0),
+	ITEM((byte) 1),
+	EXPERIENCE((byte) 2),
+	CURRENCY((byte) 3);
+
+
+	private byte id;
+
+	NimbleQuestRewardType(byte id) {
+		this.id = id;
+	}
+
+	public byte getId() {
+		return this.id;
+	}
+
+	public static NimbleQuestRewardType getById(byte id) {
+		NimbleQuestRewardType type = DEFAULT;
+		for(NimbleQuestRewardType t : values()) {
+			if(t.getId() == id) {
+				type = t;
+			}
+		}
+		return type;
+	}
+
+	public static NimbleQuestRewardType getByName(String name) {
+		NimbleQuestRewardType type = DEFAULT;
+		for(NimbleQuestRewardType t : values()) {
+      if (t.name().equalsIgnoreCase(name)) {
+        type = t;
+			}
+		}
+		return type;
+	}
+
 }
